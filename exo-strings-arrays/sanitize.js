0602f0b446bbc4077f78d152b41126ac8e18510d
pyrhaa <sboubtane@protonmail.com>
2021-02-18 10:42:43 +0100

    correction

--- exo-strings-arrays/sanitize.js
+++ exo-strings-arrays/sanitize.js
@@ -2,8 +2,6 @@ let str =
   '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n ';
 
 const trimmed = str.trim();
-const newString = trimmed.replace(
-  /AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE/gi,
-  `Alice et Bob essayent de communiquer secrètement, mais Eve veille.`
-);
-console.log(newString);
+
+let newTxt = trimmed.toLowerCase();
+console.log(newTxt);
